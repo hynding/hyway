aa9908c2088a8a08d741e55aa327b18de8542371
Steve Hynding <steve.hynding@gmail.com>
2024-11-06 14:46:11 -0800

    exports

--- languages/typescript/src/index.ts
+++ languages/typescript/src/index.ts
@@ -1,3 +1,25 @@
-export * from './data-structures'
-export * from './data-types'
-export * from './dom-helpers'
+export {
+    // arrays
+    getAdjacentMathendArray,
+    getFlattenedArray,
+    getRandomIndexFromArray,
+    getShuffledArray,
+    // matrices
+    getBoxMatrix, 
+    getColumnMatrix, 
+    getPentominoMatrix, 
+    getRowMatrix,
+} from './data-structures'
+export {
+    // floats
+    getSumFloat,
+    getProductFloat,
+    // integers
+    getMaxFloatPrecisionInteger,
+    getRandomInteger,
+} from './data-types'
+export {
+    qs, qsa, ce,
+    buildElement,
+    elementFactory,
+} from './dom-helpers'
